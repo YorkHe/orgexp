9d6036ba039313b7cc95baa85f55dd265b7a53d5
York <kiddmagician@gmail.com>
2016-07-01 08:14:24 +0800

    scoregit add .git add .

--- img_to_bin.py
+++ img_to_bin.py
@@ -34,3 +34,11 @@ def img_to_bin(bmp_name):
 if __name__ == '__main__':
     img_to_bin("0")
     img_to_bin("1")
+    img_to_bin("2")
+    img_to_bin("3")
+    img_to_bin("4")
+    img_to_bin("5")
+    img_to_bin("6")
+    img_to_bin("7")
+    img_to_bin("8")
+    img_to_bin("9")
